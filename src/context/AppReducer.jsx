3ee9b3b604c1f224a14b5e6f590bef284e5dd772
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-12 17:20:16 -0300

    fixed errors

--- src/context/AppReducer.jsx
+++ src/context/AppReducer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"; // eslint-disable-line
 
 const AppReducer = (state, action) => {
     switch (action.type) {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App";
 
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+root.render(<App />);
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
