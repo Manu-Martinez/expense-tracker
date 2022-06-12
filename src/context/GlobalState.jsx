9fa6c169873cd55e0e2261d246626ceb20a8cfc1
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-12 16:56:27 -0300

    added context

---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,27 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+    transactions: [
+    { id: 1, text: "Beer", amount: -100 },
+    { id: 2, text: "Meat", amount: -500 },
+    { id: 3, text: "Salary", amount: +10250 },
+    { id: 4, text: "Shirt", amount: -1000 },
+],
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (
+    <GlobalContext.Provider
+    value={{
+        transactions: state.transactions,
+    }}
+    >
+    {children}
+    </GlobalContext.Provider>
+);
+};
