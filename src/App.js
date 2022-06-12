2b5a33b82e2b8b9a1f8738ee9827ddbcc848a8c6
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-12 17:20:03 -0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+
 import Header from "./components/Header";
+import Balance from "./components/Balance";
+import History from "./components/History";
+import NewTransaction from "./components/NewTransaction";
+
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div className="grid grid-flow-row gap-8 bg-gray-100 p-4 font-montserrat">
-      <Header>
-        
-      </Header>
-    </div>
+    <GlobalProvider>
+      <div className="grid grid-flow-row gap-8 bg-gray-100 p-4 font-montserrat">
+        <Header />
+        <Balance />
+        <History />
+        <NewTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
