ba238ce9d378cdfbe7232b8ebf45d758c11fdaeb
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-14 18:46:44 -0300

    added Delete and Add Transaction cases

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -2,26 +2,37 @@ import React, { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 const initialState = {
-    transactions: [
-    { id: 1, text: "Beer", amount: -100 },
-    { id: 2, text: "Meat", amount: -500 },
-    { id: 3, text: "Salary", amount: +10250 },
-    { id: 4, text: "Shirt", amount: -1000 },
-],
+    transactions: []
 };
 
+// create Context
 export const GlobalContext = createContext(initialState);
 
+// Provider component
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);  // eslint-disable-line
 
-    return (
-    <GlobalContext.Provider
-    value={{
+     // Actions
+    function deleteTransaction(id) {
+    dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+    });
+}
+
+    function addTransaction(transaction) {
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction
+    });
+}
+
+
+    return ( <GlobalContext.Provider value={{
         transactions: state.transactions,
-    }}
-    >
-    {children}
-    </GlobalContext.Provider>
-);
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider> );
 };
